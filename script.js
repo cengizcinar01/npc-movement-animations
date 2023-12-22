dbdb147769370f751e6e94d56d6663431ee3ce95
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-22 02:16:58 +0100

    Add Enemy class and animation function in script.js
    A new Enemy class has been introduced to the script.js file. This class is used to create enemy objects with properties such as x, y coordinates, width, and height. An instance of the Enemy class, enemy1, has also been created.

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+
+class Enemy {
+    constructor() {
+        this.x = 10;
+        this.y = 50;
+        this.width = 100;
+        this.height = 100;
+    }
+}
+const enemy1 = new Enemy();
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemy1.x++;
+    enemy1.y++;
+    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    requestAnimationFrame(animate);
+}
+animate();
