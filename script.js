3d44a5d7c01e031870f19e76acdad1c3fe913f2e
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-22 02:03:11 +0100

    Add canvas setup in script.js
    A new file, script.js, has been created to set up the HTML canvas. This setup includes obtaining the canvas element from the DOM, getting its 2D context, and defining its width and height.

---
+++ script.js
@@ -0,0 +1,5 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
