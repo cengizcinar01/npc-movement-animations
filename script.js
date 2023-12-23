657e9256b0b157db2ea1ac2792a34b8f6f9f618f
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-23 23:44:23 +0100

    chore: Refactor enemy movement logic
    Updated and improved the randomization in Enemy class. Enemies will now spawn within canvas bounds, preventing them from spawning off-screen. Also adjusted the speed randomization. Removed static speed assignment. Instead, random speed (ranging from -2.5 to 2.5) is assigned during each update. This enhances unpredictable enemy movements.

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ class Enemy {
     constructor() {
         this.image = new Image();
         this.image.src = 'enemy1.png';
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.speed = Math.random() * 4 - 2;
+        //this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
         // animate sprites
         if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? (this.frame = 0) : this.frame++;
