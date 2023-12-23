03eadecf5af057c8865c5b26336779e0cda87c22
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-23 21:33:22 +0100

    feat: Enhance enemy generation in game script
    The enemy generation in the game script has been enhanced for a more dynamic and unpredictable gameplay. Instead of having a single enemy with fixed coordinates, the game now generates a specified number of enemies with random x and y coordinates within the canvas.

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 100;
+const enemiesArray = [];
 
 class Enemy {
     constructor() {
-        this.x = 10;
-        this.y = 50;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
         this.width = 100;
         this.height = 100;
     }
@@ -19,12 +21,16 @@ class Enemy {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
-const enemy1 = new Enemy();
 
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy());
+}
+console.log(enemiesArray);
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    enemy1.update();
-    enemy1.draw();
+    //enemy1.update();
+    //enemy1.draw();
+
     requestAnimationFrame(animate);
 }
 animate();
