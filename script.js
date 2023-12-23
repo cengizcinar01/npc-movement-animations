2fb6ad270fe98a030d9f7e45ebc37b1342cc4ec7
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-24 00:55:01 +0100

    feat: Add new Enemy class and Update enemy creation.
    A new enemy class, 'Enemy3', has been added with its unique attributes and behaviour, adding more variety to the gameplay. The 'createEnemies' function has been updated from a binary choice to a switch case, enabling the creation of diversified enemies, factoring in the new 'Enemy3' class.

--- script.js
+++ script.js
@@ -64,11 +64,66 @@ class Enemy2 {
     }
 }
 
+class Enemy3 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'enemy3.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 2;
+        this.curve = Math.random() * 200;
+    }
+    update() {
+        this.x = this.curve * Math.sin((this.angle * Math.PI) / 180) + canvas.width / 2 - this.width;
+        //this.y += this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
+        if (this.x + this.width < 0) this.x = canvas.width;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? (this.frame = 0) : this.frame++;
+        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
+
 function createEnemies() {
-    const numberOfEnemies = currentEnemyType === 'enemy1' ? 100 : 20;
+    let numberOfEnemies;
+    switch (currentEnemyType) {
+        case 'enemy1':
+            numberOfEnemies = 30;
+            break;
+        case 'enemy2':
+            numberOfEnemies = 20;
+            break;
+        case 'enemy3':
+            numberOfEnemies = 20;
+            break;
+        default:
+            numberOfEnemies = 0;
+    }
+
     enemiesArray = [];
     for (let i = 0; i < numberOfEnemies; i++) {
-        enemiesArray.push(currentEnemyType === 'enemy1' ? new Enemy1() : new Enemy2());
+        switch (currentEnemyType) {
+            case 'enemy1':
+                enemiesArray.push(new Enemy1());
+                break;
+            case 'enemy2':
+                enemiesArray.push(new Enemy2());
+                break;
+            case 'enemy3':
+                enemiesArray.push(new Enemy3());
+                break;
+        }
     }
 }
 
