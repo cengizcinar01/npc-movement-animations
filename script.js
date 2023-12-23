ac1ffacb21e361e0a9f361c6a56940790bc79754
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-23 23:38:06 +0100

    feat: update enemy class for improved sprites animation
    The enemy class in 'script.js' has been upgraded to enhance sprites animation. The image source has now been moved within the enemy class and the frame variable has been added for sprite animation. The gameFrame variable was introduced at the global scope to keep track of game animations. The width and height of the enemy were adjusted to match sprite dimensions. The 'draw' and 'update' methods were updated to incorporate these changes. The modification drastically improves the animation and overall gaming experience.

--- script.js
+++ script.js
@@ -6,26 +6,32 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = 'enemy1.png';
+let gameFrame = 0;
 
 class Enemy {
     constructor() {
+        this.image = new Image();
+        this.image.src = 'enemy1.png';
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.width = 100;
-        this.height = 100;
         this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update() {
         this.x += this.speed;
         this.y += this.speed;
+        // animate sprites
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? (this.frame = 0) : this.frame++;
+        }
     }
     draw() {
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
@@ -39,6 +45,7 @@ function animate() {
         enemy.update();
         enemy.draw();
     });
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
