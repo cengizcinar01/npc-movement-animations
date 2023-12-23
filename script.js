bd479c8d19c2772c3483d9fffc5f5492b649d791
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-24 00:35:59 +0100

    feat: add new enemy types and configuration options
    Created two distinct enemy classes instead of one, each with its unique image, size, and movement patterns. Removed static enemy count set to 100, instead, the number of enemies created now depends on the selected enemy type.
    
    Introduced select element to choose between 'enemy1' (which generates 100 enemies) and 'enemy2' (which generates 20 enemies). Added an event listener to this select element to re-create enemies upon change of selection.
    
    This gives the player more control over the game configuration, enhancing playability and strategic elements. Now the player can adjust the difficulty level and change strategy by switching enemy types.

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
-const enemiesArray = [];
-
 let gameFrame = 0;
+const enemyTypeSelect = document.getElementById('enemyType');
+let currentEnemyType = enemyTypeSelect.value;
+let enemiesArray = [];
 
-class Enemy {
+class Enemy1 {
     constructor() {
         this.image = new Image();
         this.image.src = 'enemy1.png';
-        //this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
@@ -25,7 +24,6 @@ class Enemy {
     update() {
         this.x += Math.random() * 5 - 2.5;
         this.y += Math.random() * 5 - 2.5;
-        // animate sprites
         if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? (this.frame = 0) : this.frame++;
         }
@@ -35,10 +33,50 @@ class Enemy {
     }
 }
 
-for (let i = 0; i < numberOfEnemies; i++) {
-    enemiesArray.push(new Enemy());
+class Enemy2 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'enemy2.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 7;
+    }
+    update() {
+        this.x -= this.speed;
+        this.y += this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
+        if (this.x + this.width < 0) this.x = canvas.width;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? (this.frame = 0) : this.frame++;
+        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
 }
-console.log(enemiesArray);
+
+function createEnemies() {
+    const numberOfEnemies = currentEnemyType === 'enemy1' ? 100 : 20;
+    enemiesArray = [];
+    for (let i = 0; i < numberOfEnemies; i++) {
+        enemiesArray.push(currentEnemyType === 'enemy1' ? new Enemy1() : new Enemy2());
+    }
+}
+
+enemyTypeSelect.addEventListener('change', () => {
+    currentEnemyType = enemyTypeSelect.value;
+    createEnemies();
+});
+
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     enemiesArray.forEach((enemy) => {
@@ -48,4 +86,6 @@ function animate() {
     gameFrame++;
     requestAnimationFrame(animate);
 }
+
+createEnemies();
 animate();
