77083f400a07db2682d9ddd238f92c9484ab181d
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2023-12-24 01:47:17 +0100

    Add Enemy4 Class to Game Mechanic
    A new enemy class 'Enemy4' is introduced to enhance the game difficulty and complexity. It has different movement mechanics and spawns in different quantities which are decided based on the 'currentEnemyType'. 'Enemy4' has its image, speed, and positions defined at the time of instantiation, alongside unique randomized flapSpeed and interval properties.

--- script.js
+++ script.js
@@ -95,6 +95,42 @@ class Enemy3 {
     }
 }
 
+class Enemy4 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'enemy4.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * canvas.width;
+        this.newY = Math.random() * canvas.height;
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.interval = Math.floor(Math.random() * 200 + 50);
+    }
+    update() {
+        if (gameFrame % this.interval === 0) {
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx / 70;
+        this.y -= dy / 70;
+        if (this.x + this.width < 0) this.x = canvas.width;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? (this.frame = 0) : this.frame++;
+        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
+
 function createEnemies() {
     let numberOfEnemies;
     switch (currentEnemyType) {
@@ -106,6 +142,9 @@ function createEnemies() {
             break;
         case 'enemy3':
             numberOfEnemies = 10;
+            break;
+        case 'enemy4':
+            numberOfEnemies = 30;
             break;
         default:
             numberOfEnemies = 0;
@@ -122,6 +161,9 @@ function createEnemies() {
                 break;
             case 'enemy3':
                 enemiesArray.push(new Enemy3());
+                break;
+            case 'enemy4':
+                enemiesArray.push(new Enemy4());
                 break;
         }
     }
