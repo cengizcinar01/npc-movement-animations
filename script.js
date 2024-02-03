e30d2247d8f19a29923ac93d8aeadae58f6c5ac3
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-02-03 22:46:44 +0100

    Optimize enemy3 class in script.js
    Refactored the Enemy3 class in script.js to improve readability and efficiency. Simplified the width and height calculation by introducing a 'scale' variable. Also, streamlined the update method by reducing the complexity of the x and y coordinate calculations and the logic.

--- script.js
+++ script.js
@@ -66,27 +66,24 @@ class Enemy3 {
     constructor() {
         this.image = new Image();
         this.image.src = 'enemy3.png';
+        const scale = 2;
         this.speed = Math.random() * 4 + 1;
         this.spriteWidth = 218;
         this.spriteHeight = 177;
-        this.width = this.spriteWidth / 2;
-        this.height = this.spriteHeight / 2;
+        this.width = this.spriteWidth / scale;
+        this.height = this.spriteHeight / scale;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         this.angle = Math.random() * 500;
         this.angleSpeed = Math.random() * 0.5 + 0.5;
-        //this.curve = Math.random() * 200 + 50;
     }
     update() {
-        this.x = (canvas.width / 2) * Math.cos((this.angle * Math.PI) / 200) + (canvas.width / 2 - this.width / 2);
-        this.y = (canvas.height / 2) * Math.sin((this.angle * Math.PI) / 300) + (canvas.height / 2 - this.height / 2);
+        this.x = canvas.width / 2 + (canvas.width / 2 - this.width / 2) * Math.cos((this.angle * Math.PI) / 200);
+        this.y = canvas.height / 2 + (canvas.height / 2 - this.height / 2) * Math.sin((this.angle * Math.PI) / 300);
         this.angle += this.angleSpeed;
-        if (this.x + this.width < 0) this.x = canvas.width;
-        if (gameFrame % this.flapSpeed === 0) {
-            this.frame > 4 ? (this.frame = 0) : this.frame++;
-        }
+        this.frame = gameFrame % this.flapSpeed === 0 ? (this.frame + 1) % 5 : this.frame;
     }
     draw() {
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
