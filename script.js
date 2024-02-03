2070fb29875ca84effcd2ca2ba09663110567a9b
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-02-03 22:48:57 +0100

    Scale enemy sprite and optimize target location logic
    Refactored the code to use a constant scale factor for enemy sprites, improving readability of the code. Also, optimized the logic for setting new target locations by moving the logic into a separate method.

--- script.js
+++ script.js
@@ -94,33 +94,37 @@ class Enemy4 {
     constructor() {
         this.image = new Image();
         this.image.src = 'enemy4.png';
+        const scale = 2;
         this.speed = Math.random() * 4 + 1;
         this.spriteWidth = 213;
         this.spriteHeight = 213;
-        this.width = this.spriteWidth / 2;
-        this.height = this.spriteHeight / 2;
+        this.width = this.spriteWidth / scale;
+        this.height = this.spriteHeight / scale;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
-        this.newX = Math.random() * canvas.width;
-        this.newY = Math.random() * canvas.height;
+        this.setNewTargetLocation();
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.interval = Math.floor(Math.random() * 200 + 50);
+        this.interval = Math.floor(Math.random() * 150 + 50);
     }
+
+    setNewTargetLocation() {
+        this.newX = Math.random() * (canvas.width - this.width);
+        this.newY = Math.random() * (canvas.height - this.height);
+    }
+
     update() {
         if (gameFrame % this.interval === 0) {
-            this.newX = Math.random() * (canvas.width - this.width);
-            this.newY = Math.random() * (canvas.height - this.height);
+            this.setNewTargetLocation();
         }
         let dx = this.x - this.newX;
         let dy = this.y - this.newY;
         this.x -= dx / 70;
         this.y -= dy / 70;
-        if (this.x + this.width < 0) this.x = canvas.width;
-        if (gameFrame % this.flapSpeed === 0) {
-            this.frame > 4 ? (this.frame = 0) : this.frame++;
-        }
+
+        this.frame = gameFrame % this.flapSpeed === 0 ? (this.frame + 1) % 5 : this.frame;
     }
+
     draw() {
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
